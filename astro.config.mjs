c7216d9f3a1e863095b65d6bd88e193ac0264c2f
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-12 20:33:15 +0200

    accesibility widget

--- astro.config.mjs
+++ astro.config.mjs
@@ -233,6 +233,14 @@ export default defineConfig({
             href: '/SMR-Redes/favicon.svg',
           },
         },
+        {
+          tag: 'script',
+          attrs: {
+            // Utilizamos el widget de código abierto de Sinan Isler alojado en GitHub
+            src: 'https://cdn.jsdelivr.net/gh/sinanisler/accessibility-widgets@main/dist/widget.js',
+            defer: true,
+          },
+        },
       ],
 
       sidebar: [
@@ -243,15 +251,6 @@ export default defineConfig({
         {
           label: '📚 Unidades de trabajo',
           items: unidadesSidebar,
-        },
-
-        {
-          tag: 'script',
-          attrs: {
-            // Utilizamos el widget de código abierto de Sinan Isler alojado en GitHub
-            src: 'https://cdn.jsdelivr.net/gh/sinanisler/accessibility-widgets@main/dist/widget.js',
-            defer: true,
-          },
         },
       ],
     }),
